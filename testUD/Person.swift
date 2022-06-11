019e729d7db30cac130204efd9c50983aff84565
Conner Montgomery <cmontgomery2799@gmail.com>
2022-06-10 20:00:52 -0400

    Initial Commit

---
+++ testUD/Person.swift
@@ -0,0 +1,28 @@
+//
+//  Person.swift
+//  testUD
+//
+//  Created by Conner Montgomery on 5/25/22.
+//
+
+import Foundation
+
+// Creates a class called Person
+// person contains: Name, Age, ID
+// default constructor (initializer)
+
+class Person {
+    var name: String = ""
+    var age: Int = 0
+    var id: Int = 0
+    
+    init(name:String, age:Int, id:Int){
+        self.name = name
+        self.age = age
+        self.id  = id
+        
+    }
+    
+    
+    
+}
---
+++ testUD/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  testUD
+//
+//  Created by Conner Montgomery on 5/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        /*
+        // UserDefaults in ViewDidLoad function
+        
+        // Create a new instance of UserDefaults
+        let defaults = UserDefaults.standard
+        
+        //Writing data to UserDefaults
+        
+        
+        
+        // Create key-value pairs for storage
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseTouchID")
+        defaults.set(CGFloat.pi, forKey:"Pi")
+        // More complex data type -> array
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        
+        let array2 = defaults.object(forKey:"SavedArray") as? [String] ?? [String]()
+
+        label.text = array2[0]
+        
+    */
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
+}
+
