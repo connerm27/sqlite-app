3b0378794491cf3d3bbf563f3d998b1bb110edd7
Conner Montgomery <cmontgomery2799@gmail.com>
2022-06-12 13:19:22 -0400

    completed tutorial

--- testUD/ViewController.swift
+++ testUD/ViewController.swift
@@ -7,10 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
 
     
     @IBOutlet weak var tableView: UITableView!
+    
+    let cellReuseIdentifier = "cell"
+    
+    var db:DBHelper = DBHelper()
+    
+    var persons:[Person] = []
+    
+    
+    
     
     
     override func viewDidLoad() {
@@ -42,14 +53,39 @@ class ViewController: UIViewController {
         
     */
         
+    
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        
-        
+        db.insert(id: 1, name: "Bilal", age: 24)
+              db.insert(id: 2, name: "Bosh", age: 25)
+              db.insert(id: 3, name: "Thor", age: 23)
+              db.insert(id: 4, name: "Edward", age: 44)
+              db.insert(id: 5, name: "Ronaldo", age: 34)
+              
+              persons = db.read()
         
         
         
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return persons.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
+        cell.textLabel?.text = "Name: " + persons[indexPath.row].name + ", " + "Age: " + String(persons[indexPath.row].age)
+        return cell
+        
+    }
+    
+    
+    
 
 
 }
